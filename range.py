bef793d2600168be890241240ede376d3478d5fc
Valentin Portais <val.portais@gmail.com>
2018-04-11 00:09:22 +0200

    Conditional addition

--- range.py
+++ range.py
@@ -2,11 +2,14 @@ a,b = 0,32
 i = a
 z = 0
 while i<b:
-    #add number only if it is a multiple of 3 or 5
-    if not (i%3 | i%5):
-        z = z + i
-    else:
-        print("ignoring",i)
-    i = i + 1
+  #add number if it is a multiple of 3 or 5
+  if not (i%3 | i%5):
+    z = z + i
+  #add number if multiple of 3
+  elif not (i%3):
+    z = z + i
+  elif not (i%5):
+    z = z + i
+  i = i + 1
 print("Result is",z)
 
