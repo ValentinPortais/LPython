1507932fb79154f84141580b5ad1e77f49e8405c
Valentin Portais <val.portais@gmail.com>
2018-04-26 21:27:43 +0200

    test multiple

--- conditions/range.py
+++ conditions/range.py
@@ -1,8 +1,10 @@
+# Add numbers between 'a' and 'b' which are multiple of 3 and 5 
+
 a,b = 0,32
 i = a
 z = 0
 while i<b:
-    #add
+    #find number multiple of 3 and 5
     if not (i%3 | i%5):
         z = z + i
     else:
