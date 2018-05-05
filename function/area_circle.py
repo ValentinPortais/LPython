4aefa9c304c7bc62309865cd33de403697fe4a4e
Valentin Portais <val.portais@gmail.com>
2018-05-05 20:06:03 +0200

    Change type to float

--- function/area_circle.py
+++ function/area_circle.py
@@ -3,5 +3,5 @@ def areacircle(r):
     result = pi * r**2
     return result
 
-Radius = int(input('Enter Radius'))
+Radius = float(input('Enter Radius'))
 print('The area of the circle is ',areacircle(Radius))
