3843e83c5b47c263c49dee03e11bca4bece49e7e
Valentin Portais <val.portais@gmail.com>
2018-05-06 23:50:06 +0200

    Find the max number

---
+++ function/fmax.py
@@ -0,0 +1,11 @@
+def maximum(n1,n2,n3):
+  "Return the largest number"
+  res=max(n1,n2,n3)
+  return res
+
+n1=int(input('Enter first number '))
+n2=int(input('Enter second number '))
+n3=int(input('Enter third number '))
+
+print('The largest number is',maximum(n1,n2,n3))
+
