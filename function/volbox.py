f6884558de29b48ba2d75749a6e57338392720f7
Valentin Portais <val.portais@gmail.com>
2018-05-12 21:46:22 +0200

    add defaults values for arguments

--- function/volbox.py
+++ function/volbox.py
@@ -1,12 +1,12 @@
 
-def volbox(x1, x2, x3):
+def volbox(x1=10, x2=10, x3=10):
     "Calcul volume of a parallelepiped"
     result= x1 * ( x2 * x3)
     return result
 
 x1 = float(input("Enter the first vector size"))
 x2 = float(input("Enter the second vector size"))
-x3 = float(input("Enter the third vector size"))
+#x3 = float(input("Enter the third vector size"))
 
-print("Volume is : ",volbox(x1,x2,x3))
+print("Volume is : ",volbox(x1,x2))#,#x3))
 
