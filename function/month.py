df8d3979b97857a622bf0ae1afb01ca4715c7c2a
Valentin Portais <val.portais@gmail.com>
2018-05-12 13:38:36 +0200

    Function to find a month name

---
+++ function/month.py
@@ -0,0 +1,8 @@
+def MonthName(n):
+  "Function to find a  month name"
+  mname=['January','February','March','April','May','June','July','August','September','October','November','December']
+  z=mname[n-1]
+  return z
+
+#Usage
+print(MonthName(1))
